c0a449f347aac38a481424a1a82145f9c7545471
DESTINYAS <destiny.ochonogor@gmail.com>
2022-11-29 08:03:01 +0100

    Outputting Lists

--- .history/src/Home_20221129073809.js
+++ .history/src/Home_20221129073809.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
+  const [name, setBlogs] = useState([
     { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
     { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
     {
@@ -11,14 +11,13 @@ const Home = () => {
       id: 3,
     },
   ]);
+  const handleClick = (e) => {};
+
   return (
     <div className="home">
-      {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-          <h1>{blog.title}</h1>
-          <p>Written by {blog.author}</p>
-        </div>
-      ))}
+      <h2>Homepage</h2>
+      <p>{name}</p>
+      <button onClick={handleClick}>Click me</button>
     </div>
   );
 };
